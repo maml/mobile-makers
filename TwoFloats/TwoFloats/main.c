f445fbeba7cbc4e49d93536676b8102595eb5971
Matthew Loseke <mloseke@gmail.com>
2013-09-09 22:52:08 -0500

    initial commit

---
+++ TwoFloats/TwoFloats/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  TwoFloats
+//
+//  Created by Matthew Loseke on 9/9/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    float x = 3.14;
+    float y = 42.0;
+    
+    double sumOfXandY = x + y;
+    
+    printf("The sum of the two floats x and y is %f.\n", sumOfXandY);
+}
+
