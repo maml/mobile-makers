3d42c2af4f84478c35a225c6c5c18cab69e7a216
Matthew Loseke <mloseke@gmail.com>
2013-09-11 17:17:04 -0500

    compute and display the sin of 1 radian to three decimal places

---
+++ challenge1/chapter6/Numbers/Numbers/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  Numbers
+//
+//  Created by Matthew Loseke on 9/11/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, const char * argv[])
+{
+    double y = 12345.6789;
+    printf("y is %.2f\n", y);
+    printf("y is %.2e\n", y);
+    
+    printf("sin of 1 radian is %.3f\n", sin(1));
+    
+    return 0;
+}
+
