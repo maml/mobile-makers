5f88e7ea02b5911ff4c018e39a08c213eafae719
Matthew Loseke <mloseke@gmail.com>
2013-09-15 22:07:39 -0500

    Challenge3 Section 2 answers

---
+++ challenge3/Section2/Structure/Structure/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  Structure
+//
+//  Created by Matthew Loseke on 9/15/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+// points to the Foundation Framework header file
+#import <Foundation/Foundation.h>
+
+// the first function called when the app is run, it must be present.
+int main(int argc, const char * argv[])
+
+// opening statement block for the function, main
+{
+
+    // this has something to do with memory management
+    @autoreleasepool {
+        
+        // insert code here...
+        
+        // print the statement to the console
+        NSLog(@"Hello, World!");
+        
+    }
+    
+    // main was successfull, no errors to return
+    return 0;
+
+// closing statement block for the function, main
+}
+
