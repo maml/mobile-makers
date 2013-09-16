da1d08bd9278ef19d8c6c708a0267a13167f0179
Matthew Loseke <mloseke@gmail.com>
2013-09-15 20:58:59 -0500

    Challenge3 Section 1 answers

---
+++ challenge3/Section1/FirstApp/FirstApp/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FirstApp
+//
+//  Created by Matthew Loseke on 9/15/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
