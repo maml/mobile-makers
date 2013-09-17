15fd76e0cb24ce92455c79f3c1d4cd5adabf5c21
Matthew Loseke <mloseke@gmail.com>
2013-09-16 23:22:23 -0500

    initial palindrome solution

---
+++ dailyChallenges/9-16/Palindrome/Palindrome/ViewController.m
@@ -0,0 +1,104 @@
+//
+//  ViewController.m
+//  Palindrome
+//
+//  Created by Don Bora on 4/28/13.
+//  Copyright (c) 2013 MobileMakers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    __weak IBOutlet UITextField *inputTextField;
+    __weak IBOutlet UILabel *resultLabel;
+    
+    int result;
+}
+- (IBAction)check:(id)sender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)showIsPalindrone
+{
+    resultLabel.text = @"IS a palindrone!";
+}
+
+-(void)showIsNotPalindrone
+{
+    resultLabel.text = @"IS NOT a palindrone!";
+}
+
+
+-(const char*)getCharacterArray
+{
+    return [inputTextField.text UTF8String];
+}
+
+
+-(void)showResult
+{
+    resultLabel.text = [NSString stringWithFormat:@"%i", result];
+}
+
+
+-(void)checkForPalindrome
+{
+    const char* arrayOfCharacters = [self getCharacterArray];
+    BOOL isPalindrome = NO;
+    
+    int numberOfCharacters = strlen(arrayOfCharacters);
+    
+    // Insert code in here to check the characters of the array for palindronishness
+    for (int i = 0; i < (numberOfCharacters - i); i++) {        
+        if (arrayOfCharacters[i] == arrayOfCharacters[(numberOfCharacters - 1) - i]) {
+            isPalindrome = YES;
+        } else {
+            isPalindrome = NO;
+            break;
+        }
+    }
+    
+    
+    if (isPalindrome) {
+        [self showIsPalindrone];
+    }
+    else{
+        [self showIsNotPalindrone];
+    }    
+}
+
+
+-(void)sumIntegers
+{
+    const char* arrayOfCharacters = [self getCharacterArray];
+    
+    // parse out the commas, sum up the integers and assign the answer to the variable result
+    // i.e.
+    // result =
+    
+    [self showResult];
+}
+
+- (IBAction)check:(id)sender
+{
+    [self checkForPalindrome];
+    
+    // Uncomment the following line of code and comment out the previous line of code to run summation
+    //[self sumIntegers];
+}
+@end
