7e6b9dd64af7328e13cfce8390f0cb2d64de3439
Matthew Loseke <mloseke@gmail.com>
2013-09-17 11:49:56 -0500

    update integer sum to use C's atoi for ascii to int conversions instead of brute forcing with a switch statement

--- dailyChallenges/9-16/Palindrome/Palindrome/ViewController.m
+++ dailyChallenges/9-16/Palindrome/Palindrome/ViewController.m
@@ -93,40 +93,11 @@
     result = 0;
 
     for (int i = 0; i < numberOfCharacters; i++) {
-        
-        switch (arrayOfCharacters[i]) {
-            case '0':
-                result += 0;
-                break;
-            case '1':
-                result += 1;
-                break;
-            case '2':
-                result += 2;
-                break;
-            case '3':
-                result += 3;
-                break;
-            case '4':
-                result += 4;
-                break;
-            case '5':
-                result += 5;
-                break;
-            case '6':
-                result += 6;
-                break;
-            case '7':
-                result += 7;
-                break;
-            case '8':
-                result += 8;
-                break;
-            case '9':
-                result += 9;
-                break;
-            default:
-                break;
+            
+        if (arrayOfCharacters[i] != ',')
+        {
+            result += atoi(&arrayOfCharacters[i]);
+            
         }
         
     }
