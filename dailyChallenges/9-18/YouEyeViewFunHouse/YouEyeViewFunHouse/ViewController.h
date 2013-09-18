1876ca970d129d9268122372f78d9b39b208e363
Matthew Loseke <mloseke@gmail.com>
2013-09-18 17:52:14 -0500

    update color change button

--- dailyChallenges/9-18/YouEyeViewFunHouse/YouEyeViewFunHouse/ViewController.h
+++ dailyChallenges/9-18/YouEyeViewFunHouse/YouEyeViewFunHouse/ViewController.h
@@ -10,4 +10,10 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *colorChangeButton;
+@property (weak, nonatomic) IBOutlet UIButton *moveAndChangeButton;
+
+- (IBAction)changeMyColor:(id)sender;
+- (IBAction)moveAndChange:(id)sender;
+
 @end
--- dailyChallenges/9-18/YouEyeViewFunHouse/YouEyeViewFunHouse/ViewController.m
+++ dailyChallenges/9-18/YouEyeViewFunHouse/YouEyeViewFunHouse/ViewController.m
@@ -21,4 +21,25 @@
 }
 
 
+- (IBAction)changeMyColor:(id)sender {
+    // update bg color here . . .
+    self.view.backgroundColor = [UIColor purpleColor];
+    [_colorChangeButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+}
+
+- (IBAction)moveAndChange:(id)sender {
+
+    float currentX = _moveAndChangeButton.frame.origin.x;
+    float currentY = _moveAndChangeButton.frame.origin.y;
+    
+    float currentH = _moveAndChangeButton.frame.size.height;
+    float currentW = _moveAndChangeButton.frame.size.width;
+    
+    currentY += 10.0;
+    currentW -= 20.0;
+    
+    _moveAndChangeButton.center = CGPointMake(currentX, currentY);
+
+    [_moveAndChangeButton setFrame: CGRectMake(currentX, currentY, currentW, currentH)];
+}
 @end
