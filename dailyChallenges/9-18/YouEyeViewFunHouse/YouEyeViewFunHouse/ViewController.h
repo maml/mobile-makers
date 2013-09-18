73aba14c6b7d6958b4d236b4f4f60e6f239430f5
Matthew Loseke <mloseke@gmail.com>
2013-09-18 13:14:15 -0500

    initial YouEyeViewFunHouse

---
+++ dailyChallenges/9-18/YouEyeViewFunHouse/YouEyeViewFunHouse/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ViewFunHouse
+//
+//  Created by keith Alperin on 4/30/13.
+//  Copyright (c) 2013 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ dailyChallenges/9-18/YouEyeViewFunHouse/YouEyeViewFunHouse/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  ViewFunHouse
+//
+//  Created by keith Alperin on 4/30/13.
+//  Copyright (c) 2013 Mobile Makers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
