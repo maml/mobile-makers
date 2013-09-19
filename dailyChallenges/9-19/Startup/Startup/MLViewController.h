5bbbcf6f877bb9ecf01842ae6e5dc4f148bd4bc4
Matthew Loseke <mloseke@gmail.com>
2013-09-19 13:39:47 -0500

    inital commit for Startup

---
+++ dailyChallenges/9-19/Startup/Startup/MLViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLViewController.h
+//  Startup
+//
+//  Created by Matthew Loseke on 9/19/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLViewController : UIViewController
+
+@end
---
+++ dailyChallenges/9-19/Startup/Startup/MLViewController.m
@@ -0,0 +1,29 @@
+//
+//  MLViewController.m
+//  Startup
+//
+//  Created by Matthew Loseke on 9/19/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#import "MLViewController.h"
+
+@interface MLViewController ()
+
+@end
+
+@implementation MLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
