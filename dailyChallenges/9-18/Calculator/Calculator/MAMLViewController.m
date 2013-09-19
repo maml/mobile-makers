f51fe6d417f429c51191728e4ff947d16ba46ef3
Matthew Loseke <mloseke@gmail.com>
2013-09-19 00:58:40 -0500

    initial commit for calculator

---
+++ dailyChallenges/9-18/Calculator/Calculator/MAMLViewController.m
@@ -0,0 +1,29 @@
+//
+//  MAMLViewController.m
+//  Calculator
+//
+//  Created by Matthew Loseke on 9/19/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#import "MAMLViewController.h"
+
+@interface MAMLViewController ()
+
+@end
+
+@implementation MAMLViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ dailyChallenges/9-18/Calculator/Calculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Matthew Loseke on 9/19/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MAMLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MAMLAppDelegate class]));
+    }
+}
