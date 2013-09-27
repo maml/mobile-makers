e54fc8cadb2d2067dd248bc7ee74d84dc2d6f199
Matthew Loseke <mloseke@gmail.com>
2013-09-27 15:22:55 -0500

    add new class - Game

---
+++ dailyChallenges/9-26/ViewsAndTouches/ViewsAndTouches/MLGame.h
@@ -0,0 +1,13 @@
+//
+//  MLGame.h
+//  ViewsAndTouches
+//
+//  Created by Matthew Loseke on 9/27/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MLGame : NSObject
+
+@end
---
+++ dailyChallenges/9-26/ViewsAndTouches/ViewsAndTouches/MLGame.m
@@ -0,0 +1,13 @@
+//
+//  MLGame.m
+//  ViewsAndTouches
+//
+//  Created by Matthew Loseke on 9/27/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#import "MLGame.h"
+
+@implementation MLGame
+
+@end
