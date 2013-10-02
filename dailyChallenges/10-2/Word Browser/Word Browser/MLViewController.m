57f83d919226d91e9f8cfb885e265b0dd8455531
Matthew Loseke <mloseke@gmail.com>
2013-10-02 14:07:50 -0500

    add method to viewController to construct a word - accepts two parameters of type NSString: name, and definition - instantiates and allocates new word - assigns new word's name to name param - assigns new word's definition to definition param - returns the word

--- dailyChallenges/10-2/Word Browser/Word Browser/MLViewController.m
+++ dailyChallenges/10-2/Word Browser/Word Browser/MLViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "MLViewController.h"
+#import "MLWord.h"
 
 @interface MLViewController ()
 
@@ -18,6 +19,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (MLWord *)constructWordWithName: (NSString *)name andDefinition:(NSString *)definition
+{
+    MLWord *word = [[MLWord alloc] init];
+    word.name = name;
+    word.definition = definition;
+    
+    return word;
 }
 
 @end
