7bf402731beaa6f5f16c142fd8b1fe47b671dd15
Matthew Loseke <mloseke@gmail.com>
2013-10-17 11:45:27 -0500

    basic async dispatch example

---
+++ dailyChallenges/Core Data/10-17/GCD/GCD/MLViewController.h
@@ -0,0 +1,13 @@
+//
+//  MLViewController.h
+//  GCD
+//
+//  Created by Matthew Loseke on 10/17/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLViewController : UIViewController
+
+@end
---
+++ dailyChallenges/Core Data/10-17/GCD/GCD/MLViewController.m
@@ -0,0 +1,46 @@
+//
+//  MLViewController.m
+//  GCD
+//
+//  Created by Matthew Loseke on 10/17/13.
+//  Copyright (c) 2013 Matthew Loseke. All rights reserved.
+//
+
+#import "MLViewController.h"
+
+@interface MLViewController ()
+
+@end
+
+@implementation MLViewController
+
+- (void)viewDidLoad
+{
+    NSArray *array1 = @[@1, @2, @3, @4, @5, @6, @7, @8, @9, @10];
+    NSArray *array2 = @[@1, @2, @3, @4, @5, @6, @7, @8, @9, @10];
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
+   
+    [super viewDidLoad];
+    
+    dispatch_async(queue, ^{
+        [array1 enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+            NSLog(@"element in array1 is: %@", obj);
+            sleep(arc4random() % 5);
+        }];
+    });
+
+    dispatch_async(queue, ^{
+    [array2 enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        NSLog(@"element in array2 is: %@", obj);
+        sleep(arc4random() % 5);
+    }];
+   });
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
