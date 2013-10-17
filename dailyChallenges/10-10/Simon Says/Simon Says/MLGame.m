1e45ef240d757eb7eb947a72a6913f9b6c1afb97
Matthew Loseke <mloseke@gmail.com>
2013-10-16 21:36:58 -0500

    latest Simon Says

--- dailyChallenges/10-10/Simon Says/Simon Says/MLGame.m
+++ dailyChallenges/10-10/Simon Says/Simon Says/MLGame.m
@@ -5,7 +5,7 @@
 //  Created by Matthew Loseke on 10/13/13.
 //  Copyright (c) 2013 Matthew Loseke. All rights reserved.
 //
-
+# define LEVELIZER 4
 #import "MLGame.h"
 
 @implementation MLGame
@@ -19,7 +19,8 @@
     if (self) {
         // we have the game start at level 1 and set the length of cpuSequence to two times the current level
         level = 1;
-        [self generateSequenceWithLength: level * 2];
+        //[self generateSequenceWithLength: level * 2];
+        [self generateSequenceWithLength: level * LEVELIZER];
         playerTouchCount, numberOfErrors, highScore, score = 0;
         lives = 3;
     }
@@ -33,7 +34,8 @@
 
     for (int i = 0; i < length; i++) {
         // since there are no color panels with tag 0 we bump the result up by 1 to ensure the returned value matches a color panel tag
-        int integer = (arc4random() % length) + 1;
+        // also, this range *shouldn't* really be hard-coded, need to refactor to be based on number of color panels.
+        int integer = (arc4random() % 6) + 1;
         NSString *integerToString = [NSString stringWithFormat:@"%i", integer];
         [cpuSequence addObject:integerToString];
     }
